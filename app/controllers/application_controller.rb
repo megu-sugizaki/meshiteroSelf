9aa4364a93aa1fb061fea94595bdef16b8069903
EC2 Default User <ec2-user@ip-172-31-9-47.ap-northeast-1.compute.internal>
2023-10-07 04:53:20 +0000

    meshitero復習　その1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    def after_sign_in_path_for(resource)
+        # ログインを実行したモデル（User)のデータのインスタンスが格納されている→どのuserなのか、定義を行わなくても引数でとってきてくれるということ（と思われる）（アプリ2第7章参照）
+        about_path
+    end 
+    
+    def after_sign_out_path_for(resource)
+        about_path
+    end 
+        
+        
+    protected
+    
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end 
+end
